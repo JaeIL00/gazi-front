7a8de94cdca4b2807eee91f665aebb498fdc575b
ShinJaeIl01 <ekvm555@naver.com>
2023-04-24 15:53:33 +0900

    design: 키워드 페이지 초기 구성

---
+++ src/components/templates/joinMember/InitLikeKeywordTemplate.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const InitLikeKeywordTemplate = () => {
+    return <View></View>;
+};
+export default InitLikeKeywordTemplate;
--- src/screens/InitLikeKeywordScreen.tsx
+++ src/screens/InitLikeKeywordScreen.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import { globalDefaultStyles } from '../styles/styles';
+import InitLikeKeywordTemplate from '../components/templates/joinMember/InitLikeKeywordTemplate';
 
 const InitLikeKeywordScreen = () => {
-    return <View></View>;
+    return (
+        <View style={globalDefaultStyles.background}>
+            <InitLikeKeywordTemplate />
+        </View>
+    );
 };
 
 export default InitLikeKeywordScreen;
