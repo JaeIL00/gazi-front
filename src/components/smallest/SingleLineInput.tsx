17f14634762523020fabeb646eb5f73500176a81
ShinJaeIl01 <ekvm555@naver.com>
2023-05-01 12:38:35 +0900

    feat: 키보드 숨을 때 인풋 창 Blur 기능 추가

--- src/components/smallest/SingleLineInput.tsx
+++ src/components/smallest/SingleLineInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TextInput } from 'react-native';
+import React, { RefObject, useEffect, useRef } from 'react';
+import { Keyboard, TextInput } from 'react-native';
 
 import Colors from '../../styles/Colors';
 import { SingleLineInputProps } from '../../types/types';
@@ -17,8 +17,20 @@ export const SingleLineInput = ({
     width,
     height,
 }: SingleLineInputProps) => {
+    // When keyboard hide, input is blur
+    const textInputRef = useRef() as RefObject<TextInput>;
+    const blurTextInput = () => {
+        textInputRef.current?.blur();
+    };
+    useEffect(() => {
+        const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', blurTextInput);
+        return () => {
+            keyboardDidHideListener.remove();
+        };
+    });
     return (
         <TextInput
+            ref={textInputRef}
             autoCorrect={false}
             autoCapitalize="none"
             value={value}
