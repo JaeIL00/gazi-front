b525a618921bca3afb7f23a352e55f7252d870d2
ShinJaeIl01 <ekvm555@naver.com>
2023-05-01 10:06:38 +0900

    feat: 안드로이드 기종 글자 설정 기능 제한

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { LogBox } from 'react-native';
+import { LogBox, Platform, Text, TextInput } from 'react-native';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackNavigation } from './navigations/RootStackNavigation';
 
+interface TextWithDefaultProps extends Text {
+    defaultProps?: { allowFontScaling?: boolean };
+}
+
+interface TextInputWithDefaultProps extends TextInput {
+    defaultProps?: { allowFontScaling?: boolean };
+}
+
 export const RootApp = () => {
     LogBox.ignoreAllLogs();
 
     const queryClient = new QueryClient();
+
+    if (Platform.OS === 'android') {
+        (Text as unknown as TextWithDefaultProps).defaultProps =
+            (Text as unknown as TextWithDefaultProps).defaultProps || {};
+        (Text as unknown as TextWithDefaultProps).defaultProps!.allowFontScaling = false;
+
+        (TextInput as unknown as TextInputWithDefaultProps).defaultProps =
+            (TextInput as unknown as TextInputWithDefaultProps).defaultProps || {};
+        (TextInput as unknown as TextInputWithDefaultProps).defaultProps!.allowFontScaling = false;
+    }
 
     return (
         <QueryClientProvider client={queryClient}>
