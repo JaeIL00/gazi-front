9034e154ab149549ea5ad8521d36107e29b0b1c9
ShinJaeIl01 <ekvm555@naver.com>
2023-05-11 08:47:05 +0900

    refactor: 키워드 리스트 아이템 컴포넌트 상수 추가 #16

--- src/components/organisms/KeywordsList.tsx
+++ src/components/organisms/KeywordsList.tsx
@@ -16,6 +16,8 @@ const KeywordsList = ({
     checkBorderColor,
     checkBackColor,
 }: KeywordsListProps) => {
+    const HEAD_KEYWORD = 'HEAD';
+    const SUBWAY_KEYWORD = 'SUBWAY';
     return (
         <ScrollView contentContainerStyle={keywordsListStyles.container}>
             {list.map((item, index) => (
@@ -25,26 +27,32 @@ const KeywordsList = ({
                             checkKeywordHandler(type, index, item.id);
                         }}
                         borderColor={
-                            isCheck[index] ? checkBorderColor : type === 'HEAD' ? Colors.VIOLET : Colors.TXT_GRAY
+                            isCheck[index] ? checkBorderColor : type === HEAD_KEYWORD ? Colors.VIOLET : Colors.TXT_GRAY
                         }
                         borderRadius={30}
                         borderWidth={1.1}
                         paddingHorizontal={14}
                         paddingVertical={8}
-                        backgroundColor={isCheck[index] ? checkBackColor : type === 'HEAD' ? '#F1E9FF' : Colors.WHITE}>
+                        backgroundColor={
+                            isCheck[index] ? checkBackColor : type === HEAD_KEYWORD ? '#F1E9FF' : Colors.WHITE
+                        }>
                         <>
-                            {type === 'SUBWAY' && (
+                            {type === SUBWAY_KEYWORD && (
                                 <NormalText
                                     text={item.keywordName}
                                     color={isCheck[index] ? checkTextColor : Colors.TXT_GRAY}
                                     size={16}
                                 />
                             )}
-                            {type !== 'SUBWAY' && (
+                            {type !== SUBWAY_KEYWORD && (
                                 <NormalText
                                     text={item.keywordName}
                                     color={
-                                        isCheck[index] ? checkTextColor : type === 'HEAD' ? '#7949C6' : Colors.TXT_GRAY
+                                        isCheck[index]
+                                            ? checkTextColor
+                                            : type === HEAD_KEYWORD
+                                            ? '#7949C6'
+                                            : Colors.TXT_GRAY
                                     }
                                     size={16}
                                 />
