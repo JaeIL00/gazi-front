7fa75dc61241dc9baa4e1adbb4ed01f947fa877d
ShinJaeIl01 <ekvm555@naver.com>
2023-05-12 23:18:25 +0900

    feat: 커뮤니티 스크린 포커스 조건 추가 #21

--- src/screens/CommunityScreen.tsx
+++ src/screens/CommunityScreen.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import CommunityTemplate from '../components/templates/community/CommunityTemplate';
 import { useRootNavigation } from '../navigations/RootStackNavigation';
+import { useIsFocused } from '@react-navigation/native';
 
 const CommunityScreen = () => {
     const rootNavigation = useRootNavigation();
 
-    return (
-        <View>
-            <CommunityTemplate />
-        </View>
-    );
+    const isFocused = useIsFocused();
+    return <View>{isFocused && <CommunityTemplate />}</View>;
 };
 
 export default CommunityScreen;
