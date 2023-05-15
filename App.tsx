53e199a365d4695c426402b805aeb9d2cc8b1ad9
ShinJaeIl01 <ekvm555@naver.com>
2023-05-15 14:23:53 +0900

    feat: Splash Screen 적용

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { RootApp } from './src/RootApp';
 import { appStyles } from './src/styles/styles';
 import CodePush from 'react-native-code-push';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
+    useEffect(() => {
+        setTimeout(() => {
+            SplashScreen.hide();
+        }, 100);
+    });
     return (
         <SafeAreaView style={appStyles.container}>
             <RootApp />
@@ -12,15 +18,16 @@ function App(): JSX.Element {
     );
 }
 
-const codePushOptions = {
-    checkFrequency: CodePush.CheckFrequency.ON_APP_START,
-    updateDialog: {
-        title: '...',
-        optionalUpdateMessage: '...',
-        optionalInstallButtonLabel: '업데이트',
-        optionalIgnoreButtonLabel: '아니요.',
-    },
-    installMode: CodePush.InstallMode.IMMEDIATE,
-};
+// const codePushOptions = {
+//     checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+//     updateDialog: {
+//         title: '...',
+//         optionalUpdateMessage: '...',
+//         optionalInstallButtonLabel: '업데이트',
+//         optionalIgnoreButtonLabel: '아니요.',
+//     },
+//     installMode: CodePush.InstallMode.IMMEDIATE,
+// };
 
-export default CodePush(codePushOptions)(App);
+export default App;
+// export default CodePush(codePushOptions)(App);
