324451757ee09783ac1a610575472edc2785a059
ShinJaeIl01 <ekvm555@naver.com>
2023-05-23 15:25:43 +0900

    design: 정책 및 약관 모달 닫기 디자인 수정 #11

--- src/navigations/RootStackNavigation.tsx
+++ src/navigations/RootStackNavigation.tsx
@@ -93,8 +93,8 @@ export const RootStackNavigation = () => {
         }
     };
     useLayoutEffect(() => {
-        checkAsyncStorage();
-        // SplashScreen.hide();
+        // checkAsyncStorage();
+        SplashScreen.hide();
     }, []);
 
     return (
@@ -103,12 +103,12 @@ export const RootStackNavigation = () => {
                 headerShown: false,
                 presentation: 'containedModal',
             }}>
+            <Stack.Screen name="Policies" component={PoliciesScreen} />
+            <Stack.Screen name="JoinMember" component={JoinMemberScreen} />
             <Stack.Screen name="NotLoginHome" component={NotLoginHomeScreen} />
             <Stack.Screen name="RequestPermission" component={RequestPermissionScreen} />
-            <Stack.Screen name="JoinMember" component={JoinMemberScreen} />
             <Stack.Screen name="WritePostOrComment" component={WritePostOrCommentScreen} />
             <Stack.Screen name="BottomTab" component={BottomTabNavigation} />
-            <Stack.Screen name="Policies" component={PoliciesScreen} />
             <Stack.Screen name="LikeKeywordSetting" component={LikeKeywordSettingScreen} />
             <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
             <Stack.Screen name="AccountManagement" component={AccountManagementScreen} />
