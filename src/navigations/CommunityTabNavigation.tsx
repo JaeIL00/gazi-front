eab08d7dd7db9a10af66abd3bf0e4faa52791675
ShinJaeIl01 <ekvm555@naver.com>
2023-06-19 11:18:47 +0900

    design: 커뮤니티 탭 그림자 및 프레스 컬러 삭제 #21

--- src/navigations/CommunityTabNavigation.tsx
+++ src/navigations/CommunityTabNavigation.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+import Colors from '../styles/Colors';
 import AllBoardScreen from '../screens/community/AllBoardScreen';
 import LikeBoardScreen from '../screens/community/LikeBoardScreen';
-import { CommunityTabParamList } from '../types/types';
-import Colors from '../styles/Colors';
 import { screenFont } from '../utils/changeStyleSize';
+import { CommunityTabParamList } from '../types/types';
 
 const CommunityTabNavigation = () => {
     const Tab = createMaterialTopTabNavigator<CommunityTabParamList>();
@@ -22,6 +22,10 @@ const CommunityTabNavigation = () => {
                     fontFamily: 'Pretendard-SemiBold',
                     fontSize: 16 * screenFont,
                 },
+                tabBarStyle: {
+                    shadowColor: 'transparent',
+                },
+                tabBarPressColor: 'transparent',
             }}>
             <Tab.Screen name="AllBoard" component={AllBoardScreen} options={{ title: '전체 게시판' }} />
             <Tab.Screen name="LikeBoard" component={LikeBoardScreen} options={{ title: '관심 게시판' }} />
