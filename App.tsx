ad40f2fc63a0a899d9d7b1e8a92bff9c2ee6a906
ShinJaeIl01 <ekvm555@naver.com>
2023-06-22 22:46:27 +0900

    feat: FCM 인앱 푸쉬 알림 기능 구현 #46

--- App.tsx
+++ App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { RootApp } from './src/RootApp';
 import { appStyles } from './src/styles/styles';
 import CodePush from 'react-native-code-push';
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
 function App(): JSX.Element {
+    // Create notification channel id
+    const notificationChannelId = () => {
+        PushNotification.createChannel(
+            {
+                channelId: 'channel_general', // (required)
+                channelName: 'General Notifications', // (required)
+            },
+            created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+        );
+    };
+    // FCM push notification in app
+    const foregroundNotiListener = useCallback(() => {
+        messaging().onMessage(async remoteMessage => {
+            PushNotification.localNotification({
+                message: remoteMessage.notification.body,
+                title: remoteMessage.notification.title,
+                bigPictureUrl: remoteMessage.notification.android.imageUrl,
+                smallIcon: remoteMessage.notification.android.imageUrl,
+                channelId: 'channel_general',
+            });
+            console.log('FCM push notification in app', remoteMessage);
+        });
+    }, []);
+    useEffect(() => {
+        notificationChannelId();
+        foregroundNotiListener();
+    }, []);
+
     return (
         <SafeAreaView style={appStyles.container}>
             <RootApp />
