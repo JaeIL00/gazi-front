f19c03aa87c80031188af6d9c96fae5b510612be
ShinJaeIl01 <ekvm555@naver.com>
2023-06-23 21:33:20 +0900

    design: 키워드 설정 리스트 그라데이션 색 수정 #11

--- src/components/organisms/myPage/EditMyKeyword.tsx
+++ src/components/organisms/myPage/EditMyKeyword.tsx
@@ -185,7 +185,7 @@ const EditMyKeyword = ({
         <>
             <Spacer height={10} />
             <View style={editMyKeywordStyles.mainContainer}>
-                <LinearGradient colors={['#F9F9F9', '#F9F9F900']} style={editMyKeywordStyles.upLinear} />
+                <LinearGradient colors={['#FFFFFF', '#FFFFFF00']} style={editMyKeywordStyles.upLinear} />
                 <ScrollView showsVerticalScrollIndicator={false}>
                     <Spacer height={16} />
                     <View>
@@ -238,7 +238,7 @@ const EditMyKeyword = ({
             </View>
 
             <View style={editMyKeywordStyles.bottomBox}>
-                <LinearGradient colors={['#F9F9F900', '#F9F9F9']} style={editMyKeywordStyles.downLinear} />
+                <LinearGradient colors={['#FFFFFF00', '#FFFFFF']} style={editMyKeywordStyles.downLinear} />
                 <TouchButton onPress={resetCheckedHandler} paddingHorizontal={7}>
                     <View style={editMyKeywordStyles.resetText}>
                         <Icons type="feather" name="refresh-cw" size={17} color={Colors.TXT_GRAY} />
