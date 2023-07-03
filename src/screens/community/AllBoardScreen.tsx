78f804d66422c2d3ac46494f5dcf92a8601ec707
ShinJaeIl01 <ekvm555@naver.com>
2023-07-03 20:57:06 +0900

    refactor: 커뮤니티 전체 게시판 사용하지 않는 코드 삭제 #21

--- src/screens/community/AllBoardScreen.tsx
+++ src/screens/community/AllBoardScreen.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 
 import ScreenWrapper from '../../components/organisms/ScreenWrapper';
 import AllBoardTemplate from '../../components/templates/community/AllBoardTemplate';
-import { useRootNavigation } from '../../navigations/RootStackNavigation';
 
 const AllBoardScreen = () => {
-    const rootNavigation = useRootNavigation();
-    const moveToKeywordSettingScreen = () => {
-        rootNavigation.navigate('LikeKeywordSetting', { isShortcut: true });
-    };
-
     return (
         <ScreenWrapper isPaddingHorizontal={false}>
-            <AllBoardTemplate moveToKeywordSettingScreen={moveToKeywordSettingScreen} />
+            <AllBoardTemplate />
         </ScreenWrapper>
     );
 };
